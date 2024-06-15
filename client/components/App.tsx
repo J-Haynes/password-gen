20c915d663d313f0b7fa4b10bd1b79766a9b344e
Jack Haynes <haynes.jack.stewart@gmail.com>
2024-06-15 13:53:01 +1200

    says copied

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -3,6 +3,7 @@ import Header from './Header'
 import Form from './Form'
 import Footer from './Footer'
 import Info from './Info'
+import '../styles/index.css'
 
 export default function App() {
   const [passwordInfo, setPasswordInfo] = useState({
--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 
+const lightDark = () => {
+  console.log('lightdark')
+  document.documentElement.style.setProperty('--main-bg-color', 'lightcoral')
+}
+
 export default function Header() {
   return (
     <div className="nav">
       <div className="title-div">
         <p>password app</p>
+      </div>
+      <div className="nav-div" onClick={lightDark}>
+        <ul>light/dark</ul>
       </div>
       <div className="nav-div">
         <ul>
